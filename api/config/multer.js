5501f21be4c6e66fdf899714e7d3b2e59332e611
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-07-19 20:05:12 -0300

    commit

--- api/config/multer.js
+++ api/config/multer.js
@@ -24,6 +24,7 @@ module.exports = {
     fileFilter: (req, file, callback) => {
         const allowedMimes = [
             'image/jpeg',
+            'image/jpg',
             'image/pjpeg',
             'image/png',
             'image/gif',

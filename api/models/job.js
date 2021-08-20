407831977b766c452451110442d7a96805e8014e
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2021-08-20 10:48:37 -0300

    commit

--- api/models/job.js
+++ api/models/job.js
@@ -6,49 +6,7 @@ const Mailpowerbi = require('./mailpowerbi')
 class Job {
 
     execute() {
-        const job = new CronJob('0 15 * * * *', () => {
-            try {
-                console.log('Executed Cron sucessfuly!');
-                WebScraping.init()
-            } catch (error) {
-                console.log('Error cron!' + error);
-            }
-        });
-
-        job.start()
-
-        const jobHbs = new CronJob('0 30 5 * * *', () => {
-            try {
-                console.log('Executed Cron Hbs sucessfuly!');
-                Hbs.init()
-            } catch (error) {
-                console.log('Error cron!' + error);
-            }
-        });
-
-        jobHbs.start()
-
-        const jobMail = new CronJob('0 0 * * * *', () => {
-            try {
-                console.log("Executed Mail!");
-                Mailpowerbi.listMailtoSend()
-            } catch (error) {
-                console.log('Error Mail!' + error);
-            }
-        });
-
-        jobMail.start()
-
-        const jobGoalLine = new CronJob('0 0 5 1 * *', () => {
-            try {
-                console.log("Executed Mail!");
-                Mailpowerbi.listMailtoSend()
-            } catch (error) {
-                console.log('Error Mail!' + error);
-            }
-        });
-
-        jobGoalLine.start()
+ 
     }
 }
 

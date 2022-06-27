48e1e22bc2fc8556fba812c4ce8397344ffac577
Raphael Martinez <70238369+raphamartinez@users.noreply.github.com>
2022-06-27 11:29:50 -0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: __dirname + '\\.env', encoding: 'utf8' });
+require('events').EventEmitter.prototype._maxListeners = 100;
 
 const customExpress = require('./api/config/customExpress')
 const appLocals = require("./api/config/app.locals");
